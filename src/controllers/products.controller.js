83466c1691821dd283f469b33a65fa1aa9f3efe9
sarfarazakram576 <sarfaraz.akram055@gmail.com>
2025-09-09 01:30:25 +0600

    make products api to delete and get products

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,6 +1,33 @@
+import { ObjectId } from "mongodb";
 import connectDB from "../config/db.js";
 
 const { sellers, products } = await connectDB();
+
+export const getMyProduct = async (req, res) => {
+  try {
+    let { page = 1, limit = 12, email } = req.query;
+    page = parseInt(page);
+    limit = parseInt(limit);
+    const skip = (page - 1) * limit;
+
+    const store = await sellers.findOne({ email });
+    const query = { storeId: store._id.toString() };
+
+    const total = await products.countDocuments(query);
+    const myProducts = await products
+      .find(query)
+      .sort({ addedAt: -1 })
+      .skip(skip)
+      .limit(limit)
+      .toArray();
+
+    res.send({ myProducts, total });
+  } catch (err) {
+    res
+      .status(500)
+      .send({ message: "Failed to fetch products", error: err.message });
+  }
+};
 
 export const addProduct = async (req, res) => {
   try {
@@ -18,5 +45,26 @@ export const addProduct = async (req, res) => {
     });
   } catch (_) {
     res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const query = { _id: new ObjectId(id) };
+    const product = await products.findOne(query);
+
+    if (product.sellerEmail !== req.query.email) {
+      return res.status(403).send({
+        message: "You are not allowded to delete other seller's product",
+      });
+    }
+
+    const result = await products.deleteOne(query);
+    res.send(result);
+  } catch (err) {
+    res
+      .status(500)
+      .send({ message: "Failed to delete product", error: err.message });
   }
 };
--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -1,10 +1,14 @@
 import express from "express";
 import { verifyJwt } from "../middleware/verifyJwt.middleware.js";
 import { verifySeller } from "../middleware/verifySeller.middleware.js";
-import { addProduct } from "../controllers/products.controller.js";
+import { addProduct, deleteProduct, getMyProduct } from "../controllers/products.controller.js";
 
 const productsRouter = express.Router();
 
+productsRouter.get("/my", verifyJwt, verifySeller, getMyProduct);
+
 productsRouter.post("/", verifyJwt, verifySeller, addProduct);
+
+productsRouter.delete("/:id", verifyJwt, verifySeller, deleteProduct);
 
 export default productsRouter;
