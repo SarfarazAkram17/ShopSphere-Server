7ebcd307908d64d609476404391dcad97ad90cd7
sarfarazakram576 <sarfaraz.akram055@gmail.com>
2025-11-13 18:46:08 +0600

    make api to confirm order after select cod payment

--- src/routes/orders.route.js
+++ src/routes/orders.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { verifyJwt } from "../middleware/verifyJwt.middleware.js";
 import { verifyCustomer } from "../middleware/verifyCustomer.middleware.js";
-import { createOrder } from "../controllers/orders.controller.js";
+import { confirmOrder, createOrder } from "../controllers/orders.controller.js";
 
 const ordersRouter = express.Router();
 
-ordersRouter.post('/', verifyJwt, verifyCustomer, createOrder)
+ordersRouter.post("/", verifyJwt, verifyCustomer, createOrder);
+ordersRouter.patch("/:orderId/confirm", verifyJwt, verifyCustomer, confirmOrder);
 
 export default ordersRouter;
